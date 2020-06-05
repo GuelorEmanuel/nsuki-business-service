b93efad9aa176b0c04ef7ea3b30e28d1f5c53c66
gueloremanuel <ncstech07@gmail.com>
2020-06-05 14:29:04 -0400

    Add dashboard access on all environments

--- lib/nsuki_business_service_web/router.ex
+++ lib/nsuki_business_service_web/router.ex
@@ -1,8 +1,20 @@
 defmodule NsukiBusinessServiceWeb.Router do
   use NsukiBusinessServiceWeb, :router
+  import Plug.BasicAuth
+  import Phoenix.LiveDashboard.Router
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :admins_only do
+    plug :basic_auth, username:  "#{Application.get_env(:nsuki_business_service, NsukiBusinessServiceWeb.Endpoint)[:username]}",
+                      password:  "#{Application.get_env(:nsuki_business_service, NsukiBusinessServiceWeb.Endpoint)[:password]}"
+  end
+
+  scope "/" do
+    pipe_through :admins_only  
+    live_dashboard "/dashboard"
   end
 
   scope "/api", NsukiBusinessServiceWeb do
