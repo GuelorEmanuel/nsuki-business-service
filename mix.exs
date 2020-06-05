41bb9ee33e537af81d08a859108f590324d369ff
gueloremanuel <ncstech07@gmail.com>
2020-06-05 01:15:19 -0400

    Update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule NsukiBusinessService.MixProject do
     [
       app: :nsuki_business_service,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.10.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
