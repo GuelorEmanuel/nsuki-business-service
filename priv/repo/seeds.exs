021de76b2c027b47a6180456dc11a468573c3434
gueloremanuel <ncstech07@gmail.com>
2020-09-22 16:04:19 -0400

    Populate Deposit

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,16 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias NsukiBusinessService.Services.Deposit
+
+no_deposit =
+  %Deposit{type: "No Deposit"}
+  |> NsukiBusinessService.Repo.insert!()
+
+fixed =
+  %Deposit{type: "Fixed"}
+  |> NsukiBusinessService.Repo.insert!()
+
+percentage =
+  %Deposit{type: "Percentage"}
+  |> NsukiBusinessService.Repo.insert!()
