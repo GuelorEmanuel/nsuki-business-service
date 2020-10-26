893a49e647a97b42771c1114e1a4d1ad47a40a33
Guelor Emanuel <gueloremanuel@Guelors-MacBook-Pro.local>
2020-10-25 20:51:15 -0400

    Add Hammox.defmock

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,3 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(NsukiBusinessService.Repo, :manual)
+Hammox.defmock(NsukiBusinessService.MockCalendarServiceBehaviour, for: Behaviour.NsukiBusinessService.CalendarServiceBehaviour)
